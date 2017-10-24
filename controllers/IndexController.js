0037532b5a487cd4aa5608753e3c7e097cafd45c
farhan <mf.tahir@outlook.com>
2017-10-24 22:17:59 +0500

    Started getting dynamic projects and experiences.

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,3 +1,9 @@
-exports.index = (req, res) => {
-	res.render('index');
+const mongoose = require('mongoose');
+
+const Project = mongoose.model('Project');
+const Experience = mongoose.model('Experience');
+exports.index = async (req, res) => {
+	const projects = await Project.find().sort('sort');
+	const experiences = await Experience.find().sort('sort');
+	res.render('index', { projects, experiences });
 };
