2ba0974ee2a070f3534d723efdbc7144c04c7f4d
farhan <mf.tahir@outlook.com>
2017-10-29 13:09:22 +0500

    bootsrap datepicker added

--- public/admin/js/index.js
+++ public/admin/js/index.js
@@ -2,6 +2,7 @@ import '../vendor/bootstrap/css/bootstrap.min.css';
 import '../vendor/datatables/dataTables.bootstrap4.css';
 import '../css/sb-admin.css';
 import '../vendor/font-awesome/css/font-awesome.min.css';
+import '../vendor/bootstrap-datepicker/css/bootstrap-datepicker.min.css';
 
 import '../vendor/jquery/jquery';
 import '../vendor/popper/popper';
@@ -9,5 +10,6 @@ import '../vendor/bootstrap/js/bootstrap';
 import '../vendor/jquery-easing/jquery.easing.min';
 import '../vendor/datatables/dataTables.bootstrap4';
 import '../vendor/datatables/jquery.dataTables';
-import './sb-admin.min';
+import '../vendor/bootstrap-datepicker/js/bootstrap-datepicker.min';
+import './sb-admin';
 import './sb-admin-datatables.min';
